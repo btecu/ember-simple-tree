8811ff98e5c55e7b3360cce7bb31a4ed0624db86
Aaron Heidorn <aaron.heidorn@healthcaresource.com>
2018-03-30 08:42:17 -0400

    using programatic selection/styling (keyboard support)

--- addon/components/x-tree-node.js
+++ addon/components/x-tree-node.js
@@ -1,19 +1,28 @@
 import Component from '@ember/component';
+import { computed }  from '@ember/object';
 import layout from '../templates/components/x-tree-node';
 
 export default Component.extend({
   layout,
-  classNameBindings: ['model.isSelected:tree-highlight'],
+  classNameBindings: ['model.isSelected:tree-highlight', 'isChosen:tree-chosen'],
+
+  isChosen: computed('model.id', 'chosenId', function() {
+    let chosenId = this.get('chosenId');
+    return chosenId ? this.get('model.id') === chosenId : false;
+  }),
 
   click() {
     this.attrs.select(this.get('model'));
   },
-
   mouseEnter() {
+    this.set('model.isSelected', true);
     let hover = this.get('hover');
     if (hover) {
       hover(this.get('model'));
     }
+  },
+  mouseLeave() {
+    this.set('model.isSelected', false);
   },
 
   actions: {
