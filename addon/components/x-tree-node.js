aa8dad5c9a97394f39181a7634dd9f828b424d3a
Teddy Coleman <teddy.coleman@datafox.co>
2018-08-09 16:10:06 -0700

    adds syncing checkboxes to parents/children and display indeterminate checkbox

--- addon/components/x-tree-node.js
+++ addon/components/x-tree-node.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { computed }  from '@ember/object';
+import { computed, observer }  from '@ember/object';
 import layout from '../templates/components/x-tree-node';
 
 export default Component.extend({
@@ -9,6 +9,30 @@ export default Component.extend({
   isChosen: computed('model.id', 'chosenId', function() {
     return this.get('model.id') === this.get('chosenId');
   }),
+
+  syncCheckbox: false,
+
+  __childrenCheckboxObserver: observer('model.children.@each.isChecked', function() {
+    if (this.get('syncCheckbox')) {
+      const children = this.get('model.children');
+      const allChildrenAreChecked = children.every(this._isChecked);
+      const someChildrenAreChecked = children.some(this._isChecked);
+      if (someChildrenAreChecked && !allChildrenAreChecked) {
+        this.set('model.isChecked', true);
+        this.set('model.isIndeterminate', true);
+      } else if (someChildrenAreChecked) {
+        this.set('model.isChecked', true);
+        this.set('model.isIndeterminate', false);
+      } else {
+        this.set('model.isChecked', false);
+        this.set('model.isIndeterminate', false);
+      }
+    }
+  }),
+
+  _isChecked(node) {
+    return node.isChecked;
+  },
 
   click() {
     let select = this.get('onSelect');
@@ -33,11 +57,25 @@ export default Component.extend({
     }
   },
 
+  setChildCheckboxesRecursively(parentNode, checkValue) {
+    const children = parentNode.children;
+    if (children.length) {
+      children.setEach('isChecked', checkValue);
+      children.forEach((child) => {
+        this.setChildCheckboxesRecursively(child, checkValue);
+      });
+    }
+  },
+
   actions: {
     toggleCheck(event) {
       event.stopPropagation();
       this.toggleProperty('model.isChecked');
 
+      if (this.get('syncCheckbox')) {
+        const checkValue = this.get('model.isChecked');
+        this.setChildCheckboxesRecursively(this.get('model'), checkValue);
+      }
       let check = this.get('onCheck');
       if (check) {
         check(this.get('model'));
