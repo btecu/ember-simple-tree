36935f33e9fbb6df8e8cf1cd5969e81413c3f3fc
Gaurav Munjal <gaurav@peeriq.com>
2019-02-19 11:30:08 -0500

    fix generated tests

---
+++ tests/integration/components/light-tree-branch-test.js
@@ -0,0 +1,31 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | light-tree-branch', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{light-tree-branch}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    this.set('tree', [{
+      id: 1,
+      isVisible: true
+    }]);
+
+    // Template block usage:
+    await render(hbs`
+      {{#light-tree-branch model=tree}}
+        template block text
+      {{/light-tree-branch}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
