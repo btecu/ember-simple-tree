00f02dcc3d8601089efa337dc90fb4753ddf96bc
Aaron Heidorn <aaron.heidorn@healthcaresource.com>
2019-10-23 10:34:57 -0400

    Fix checkbox in IE

--- addon/components/x-tree-checkbox.js
+++ addon/components/x-tree-checkbox.js
@@ -4,5 +4,11 @@ import layout from '../templates/components/x-tree-checkbox';
 export default Component.extend({
   layout,
   tagName: 'span',
-  classNames: ['tree-checkbox']
+  classNames: ['tree-checkbox'],
+
+  actions: {
+    // IE requires a change action to be set or it will
+    // attempt to mutate the checked attribute
+    noop() { }
+  }
 });
