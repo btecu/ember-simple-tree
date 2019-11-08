afa39cc0df2728fe5ca15713e81ec8436b6af10f
Gaurav Munjal <Gaurav0@aol.com>
2019-11-08 03:28:52 -0500

    add tests

--- tests/integration/components/x-tree-node-test.js
+++ tests/integration/components/x-tree-node-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | x-tree-node', function(hooks) {
@@ -22,5 +22,67 @@ module('Integration | Component | x-tree-node', function(hooks) {
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  test('select event', async function(assert) {
+    this.selected = false;
+    this.model = {
+      name: 'a',
+      children: []
+    };
+    this.set('onSelect', () => {
+      this.selected = !this.selected;
+    });
+
+    await render(hbs`{{x-tree-node model=model onSelect=(action onSelect)}}`);
+
+    await click('.tree-toggle');
+
+    assert.equal(this.selected, true, 'selected');
+
+    await click('.tree-toggle');
+
+    assert.equal(this.selected, false, 'unselected');
+  });
+
+  test('contextmenu event', async function(assert) {
+    this.rightClicked = false;
+    this.model = {
+      name: 'a',
+      children: []
+    };
+    this.set('onContextMenu', () => {
+      this.rightClicked = true;
+    });
+
+    await render(hbs`{{x-tree-node model=model onContextMenu=(action onContextMenu)}}`);
+
+    await triggerEvent('.tree-toggle', 'contextmenu');
+
+    assert.equal(this.rightClicked, true, 'right click detected');
+  });
+
+  test('onHover and onHoverOut events', async function(assert) {
+    this.hovering = false;
+    this.model = {
+      name: 'a',
+      children: []
+    };
+    this.set('onHover', () => {
+      this.hovering = true;
+    });
+    this.set('onHoverOut', () => {
+      this.hovering = false;
+    });
+
+    await render(hbs`{{x-tree-node model=model onHover=(action onHover) onHoverOut=(action onHoverOut)}}`);
+
+    await triggerEvent('.tree-toggle', 'mouseenter');
+
+    assert.equal(this.hovering, true, 'hovering');
+
+    await triggerEvent('.tree-toggle', 'mouseleave');
+
+    assert.equal(this.hovering, false, 'hover out');
   });
 });
